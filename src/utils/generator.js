72a7598be310848c24f1a37f0b28551991352a95
Erroll Sipagan <errollsipagan@gmail.com>
2025-06-05 02:08:42 +0800

    Implement partials for maintainability

--- src/utils/generator.js
+++ src/utils/generator.js
@@ -2,6 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const Handlebars = require('handlebars');
 
+const partialsDir = path.join(__dirname, '../templates/partials');
+fs.readdirSync(partialsDir).forEach(filename => {
+    const matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) return;
+    const name = matches[1];
+    const filepath = path.join(partialsDir, filename);
+    const template = fs.readFileSync(filepath, 'utf8');
+    Handlebars.registerPartial(name, template);
+});
+
 function generateResume(details) {
     const templatePath = path.join(__dirname, '../templates/resumeTemplate.hbs');
     const templateSource = fs.readFileSync(templatePath, 'utf-8');
